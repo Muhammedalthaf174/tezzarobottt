eaa6036a7073f190dde03b871b53e27c23de30ec
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-05-31 15:48:10 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -75,7 +75,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 ────「 Tezza Robot 」────
-*Hᴇʟʟᴏ! {}*   
+*Hᴇʟʟᴏ ɪ'ᴍ ᴛᴇᴢᴢᴀ*   
 ✪ *I'ᴍ ᴘᴏᴡᴇʀғᴜʟ Gʀᴏᴜᴘ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ 🔥*
 ✪ *I'ᴍ Vᴇʀʏ Fᴀꜱᴛ Aɴᴅ Mᴏʀᴇ Eꜰꜰɪᴄɪᴇɴᴛ I Pʀᴏᴠɪᴅᴇ Aᴡᴇꜱᴏᴍᴇ Fᴇᴀᴛᴜʀᴇꜱ!*
 ─────────────────────
