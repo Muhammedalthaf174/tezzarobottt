ca90cdaab592c66b77d945edfd20ddd06fdfb7bf
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-06-03 18:47:09 +0530

    Update __polling.py

--- MashaRoBot/modules/__polling.py
+++ MashaRoBot/modules/__polling.py
@@ -393,25 +393,4 @@ async def stop(event):
                 await event.reply("Seems like you haven't created any poll yet !")
 
 
-__help__ = """
-You can now send polls anonymously with Masha
-Here is how you can do it:
-**Parameters** -
- ❍ poll-id - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
- ❍ question - the question you wanna ask
- ❍ <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
- ❍ <True/False>(2) - public votes
- ❍ <True/False>(3) - multiple choice
-**Syntax** -
-❍ `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
-**Examples** -
-❍ `/poll 12345 | am i cool? | False False False yes no`
-❍ `/poll 12345 | am i cool? | True@1 False False yes no`
-**To stop a poll**
-Reply to the poll with `/stoppoll <poll-id>` to stop the poll
-**NOTE**
-If you have forgotten your poll id or deleted the poll so that you can't stop the previous poll type `/forgotpollid`, this will reset the poll id, you will have no access to the previous poll !
-"""
 
-
-__mod_name__ = "POLLING"
